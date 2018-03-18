b6330c26ffc802d5bbd1df694a4fe1af062ae8b7
Lindell Carter <lindellcarternyc@gmail.com>
2018-03-17 22:49:02 -0400

    cleanup create-react-app artifacts and create app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
 import * as React from 'react'
-import './App.css'
 
-class App extends React.Component {
+import { 
+  // Container,
+  Sidebar, SidebarPushable, SidebarPusher,
+} from 'semantic-ui-react'
+
+import TopBar from './components/top-bar'
+import MarkdownPreview from './components/markdown-preview'
+
+interface AppState {
+  settingsOpen: boolean
+}
+class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props)
+
+    this.state = {
+      settingsOpen: false
+    }
+  }
+  onClickTopbarButton = () => {
+    console.dir(this.state)
+  }
+
+  toggleSettingsOpen = () => {
+    const settingsOpen = !this.state.settingsOpen
+    this.setState({ settingsOpen })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <SidebarPushable>
+        <Sidebar visible={this.state.settingsOpen}>Menu</Sidebar>
+        <SidebarPusher>
+          <TopBar onClick={this.toggleSettingsOpen}/>
+          <MarkdownPreview />
+        </SidebarPusher>
+      </SidebarPushable>
     )
   }
 }
